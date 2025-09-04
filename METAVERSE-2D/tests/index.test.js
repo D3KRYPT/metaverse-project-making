f5495da5b515a2c5e30d0644370617ca791bf32a
Saumyadweepta Paul <92589899+D3KRYPT@users.noreply.github.com>
2025-09-04 18:00:41 +0000

    authentication endpoint done

--- METAVERSE-2D/tests/index.test.js
+++ METAVERSE-2D/tests/index.test.js
@@ -1,10 +1,72 @@
+const { default: axios } = require("axios");
+
 function sum(a, b){
     return a + b
 }
-describe("Authentication", () => {
-test('adds 1 + 2 to equal 3', () => {
-    let ans = sum(1,2)
-  expect(ans).toBe(3);
+
+const BACKEND_URL = "http://localhost:3000"
+describe("Authentication",  () => {
+test('User is able to sign up only once', async () => {
+    const username = "paul" + Math.random();
+    const password = "123456";
+
+    const response = await axios.post(`${BACKEND_URL}api/v1/signup`,{
+      username,
+      password,
+      type :"admin"  
+    })
+
+    expect(response.statusCode).toBe(200)
+    const updatedResponse = await axios.post(`${BACKEND_URL}api/v1/signup`,{
+      username,
+      password,
+      type :"admin"  
+    })
+     expect(updatedResponse.statusCode).toBe(400)
 });
- 
+
+test('Signup fails when the username is empty', async() => {
+    const username = `kirat-${Math.random()}`
+    const password = "123456"
+
+    const response = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+      password
+    })
+    expect(response.statusCode).toBe(400)
+}) 
+
+test('Signin succeeds when the username and password are correct', async() => {
+
+const username = `kirat-${Math.random()}`
+const password = "123456"
+
+await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+  username,
+  password
+});
+
+const response = await axios.post(`${BACKEND_URL}/api/v1/signin`, {
+  username, 
+  password
+});
+expect(response.statusCode).toBe(200)
+expect(response.body.token).toBeDefined()
+})
+
+test('Signin fails when the username and password are in-correct', async() => {
+
+const username = `kirat-${Math.random()}`
+const password = "123456"
+
+await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+  username,
+  password
+});
+
+const response = await axios.post(`${BACKEND_URL}/api/v1/signin`, {
+  username : "wrongusername", 
+  password
+});
+expect(response.statusCode).toBe(403)
+})
 })
